f790038360c9376021a4a372790f7826eb80f292
Simon Lundström <simmel@soy.se>
2015-01-29 08:50:56 +0100

    Add function to check for err

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/user"
 	"path"
 )
+
+func check_err(err error) {
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		os.Exit(1)
+	}
+}
 
 func main() {
 	usr, err := user.Current()
